eaf798129df4c4aa13052c8884f53d7e29141433
douglasBaumgratz <douglas.baumgratz@gmail.com>
2018-11-04 18:29:34 -0200

    Arrumando identação da query DAO

--- src/java/br/ufjf/dcc078/Persistencia/PedidoDAO.java
+++ src/java/br/ufjf/dcc078/Persistencia/PedidoDAO.java
@@ -17,81 +17,78 @@ import java.sql.Statement;
  * @author douglas
  */
 public class PedidoDAO {
+
     private static PedidoDAO instance = new PedidoDAO();
+
+    private PedidoDAO() {
+    }
+
+    ;
     
-    private PedidoDAO() {};
-    
-    public static PedidoDAO getInstance(){
+    public static PedidoDAO getInstance() {
         return instance;
     }
- 
+
     public void save(Pedido pedido) throws
-            SQLException, ClassNotFoundException 
-    {
+            SQLException, ClassNotFoundException {
         Connection conn = null;
         Statement st = null;
-        
-        try 
-        {
+
+        try {
             conn = (Connection) DatabaseLocator.getInstance().getConnection();
             st = conn.createStatement();
-            st.execute("insert into pedido(id_usuario, titulo, descricao, status, promocao) " + 
-                    "values('" + pedido.getUsuario().getId()+ "','" + pedido.getTitulo()+ "', '"+pedido.getDescricao()+"', '"+pedido.getStatus()+"', '"+pedido.getPromocao()+"')");
-        }
-        catch (SQLException e) 
-        {
+            st.execute("insert into pedido(id_usuario, titulo, descricao, status, promocao) "
+                    + "values('"
+                    + pedido.getUsuario().getId() + "','"
+                    + pedido.getTitulo() + "', '"
+                    + pedido.getDescricao() + "', '"
+                    + pedido.getStatus() + "', '"
+                    + pedido.getPromocao() + "')"
+            );
+        } catch (SQLException e) {
             throw e;
-        }
-        finally 
-        {
+        } finally {
             closeResources(conn, st);
-        }       
+        }
     }
-    
+
     public String read(Usuario usuario) throws
-            SQLException, ClassNotFoundException 
-    {
+            SQLException, ClassNotFoundException {
         Connection conn = null;
         Statement stmt = null;
         String id_pedido = null;
-        
-        try 
-        {
+
+        try {
             conn = (Connection) DatabaseLocator.getInstance().getConnection();
             stmt = conn.createStatement();
-          
+
             String query = "select id_pedido from pedido where id_usuario = '%"
-                        + usuario.getId()
-                        + "%'";
-            
+                    + usuario.getId()
+                    + "%'";
+
             ResultSet rs = stmt.executeQuery(query);
-            
-            while (rs.next()) 
-            {
-                id_pedido = rs.getString("id_pedido");                
+
+            while (rs.next()) {
+                id_pedido = rs.getString("id_pedido");
             }
-            
+
             return id_pedido;
-        }
-        catch (SQLException e) 
-        {
+        } catch (SQLException e) {
             throw e;
-        }
-        finally 
-        {
+        } finally {
             closeResources(conn, stmt);
-        }       
-    }
-    
-    public void closeResources(Connection conn, Statement st)
-    {
-        try 
-        {
-            if(st!=null) st.close();
-            if(conn!=null) conn.close();
         }
-        catch(SQLException e)
-        {
+    }
+
+    public void closeResources(Connection conn, Statement st) {
+        try {
+            if (st != null) {
+                st.close();
+            }
+            if (conn != null) {
+                conn.close();
+            }
+        } catch (SQLException e) {
             e.printStackTrace();
         }
     }
--- src/java/br/ufjf/dcc078/Persistencia/UsuarioDAO.java
+++ src/java/br/ufjf/dcc078/Persistencia/UsuarioDAO.java
@@ -8,93 +8,87 @@ package br.ufjf.dcc078.Persistencia;
 /**
  *
  * @author douglas
-*/
-
+ */
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import  br.ufjf.dcc078.Dominio.Usuario;
+import br.ufjf.dcc078.Dominio.Usuario;
 
 public class UsuarioDAO {
-       
+
     private static UsuarioDAO instance = new UsuarioDAO();
+
+    private UsuarioDAO() {
+    }
+
+    ;
     
-    private UsuarioDAO() {};
-    
-    public static UsuarioDAO getInstance(){
+    public static UsuarioDAO getInstance() {
         return instance;
     }
- 
+
     public void save(Usuario usuario) throws
-            SQLException, ClassNotFoundException 
-    {
+            SQLException, ClassNotFoundException {
         Connection conn = null;
         Statement st = null;
-        
-        try 
-        {
+
+        try {
             conn = (Connection) DatabaseLocator.getInstance().getConnection();
             st = conn.createStatement();
-            st.execute("insert into usuario(nomeCompleto, nomeUsuario, email, senha) " + 
-                    "values('" + usuario.getNomeCompleto()+ "','" + usuario.getNomeUsuario()+ "', '"+usuario.getEmail()+"', '"+usuario.getSenha()+"')");
-        }
-        catch (SQLException e) 
-        {
+            st.execute("insert into usuario(nomeCompleto, nomeUsuario, email, senha) "
+                    + "values('"
+                    + usuario.getNomeCompleto() + "','"
+                    + usuario.getNomeUsuario() + "', '"
+                    + usuario.getEmail() + "', '"
+                    + usuario.getSenha() + "')"
+            );
+        } catch (SQLException e) {
             throw e;
-        }
-        finally 
-        {
+        } finally {
             closeResources(conn, st);
-        }       
+        }
     }
-    
+
     public String read(Usuario usuario) throws
-            SQLException, ClassNotFoundException 
-    {
+            SQLException, ClassNotFoundException {
         Connection conn = null;
         Statement stmt = null;
         String email = null;
-        
-        try 
-        {
+
+        try {
             conn = (Connection) DatabaseLocator.getInstance().getConnection();
             stmt = conn.createStatement();
-          
+
             String query = "select email from usuario where nome_completo like '%"
-                        + usuario.getNomeCompleto()
-                        + "%'";
-            
+                    + usuario.getNomeCompleto()
+                    + "%'";
+
             ResultSet rs = stmt.executeQuery(query);
-            
-            while (rs.next()) 
-            {
-                email = rs.getString("email");                
+
+            while (rs.next()) {
+                email = rs.getString("email");
             }
-            
+
             return email;
-        }
-        catch (SQLException e) 
-        {
+        } catch (SQLException e) {
             throw e;
-        }
-        finally 
-        {
+        } finally {
             closeResources(conn, stmt);
-        }       
-    }
-    
-    public void closeResources(Connection conn, Statement st)
-    {
-        try 
-        {
-            if(st!=null) st.close();
-            if(conn!=null) conn.close();
         }
-        catch(SQLException e)
-        {
+    }
+
+    public void closeResources(Connection conn, Statement st) {
+        try {
+            if (st != null) {
+                st.close();
+            }
+            if (conn != null) {
+                conn.close();
+            }
+        } catch (SQLException e) {
             e.printStackTrace();
         }
     }
-    
+
 }
