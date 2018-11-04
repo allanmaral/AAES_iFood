35c9c1ec0f8f1693c5f9b29894c3155d756c7944
Allan Amaral <allanmaral@hotmail.com>
2018-11-04 15:34:16 -0200

    FrontController e ActionFactory implementados

---
+++ src/java/br/ufjf/dcc078/Servlet/ActionFactory.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.ufjf.dcc078.Servlet;
+
+/**
+ *
+ * @author allan
+ */
+public class ActionFactory {
+    public static Action create(String action) {
+        Action actionObject = null;
+        String nomeClasse = "br.ufjf.dcc078.Servlet." + action + "Action";
+        Class classe = null;
+        Object objeto = null;
+        
+        try {
+            classe = Class.forName(nomeClasse);
+            objeto = classe.newInstance();
+        } catch (ClassNotFoundException | 
+                 IllegalAccessException | 
+                 InstantiationException ex) {
+            return null;
+        }
+        
+        if(!(objeto instanceof Action)) return null;
+        actionObject = (Action)objeto;
+        return actionObject;
+    }
+}
