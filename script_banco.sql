f05e50fae1d00526166a7d097b1b73111dd29a7e
douglasBaumgratz <douglas.baumgratz@gmail.com>
2018-11-07 16:53:20 -0200

    Consertando script_banco.sql

--- script_banco.sql
+++ script_banco.sql
@@ -1,7 +1,6 @@
 DROP TABLE pedido;
 DROP TABLE usuario;
-
-
+DROP TABLE promocao;
 
 CREATE TABLE usuario (
     id_usuario INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
@@ -11,53 +10,32 @@ CREATE TABLE usuario (
     senha VARCHAR(20) NOT NULL    
 );
 
+CREATE TABLE promocao (
+    id_promocao INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    codigo VARCHAR(50) NOT NULL,
+    desconto DECIMAL(10,1)
+);
+
 CREATE TABLE pedido (
     id_pedido INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     id_usuario INTEGER NOT NULL,
-    id_promocao INTEGER,
+    id_promocao INTEGER NOT NULL,
     titulo VARCHAR(50) NOT NULL,
     estado VARCHAR(15) NOT NULL,    
-    FOREIGN KEY (id_usuario) REFERENCES usuario(id_usuario) ON DELETE CASCADE
-    FOREIGN KEY (id_promocao) REFERENCES usuario(id_promocao) ON DELETE CASCADE
+    FOREIGN KEY (id_usuario) REFERENCES usuario(id_usuario) ON DELETE CASCADE,
+    FOREIGN KEY (id_promocao) REFERENCES promocao(id_promocao) ON DELETE CASCADE
 );
 
-CREATE TABLE promocao (
-	id_promocao INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-    codigo VARCHAR(50) NOT NULL,
-    desconto DECIMAL
-);
+INSERT INTO usuario(nome_completo, nome_usuario, email, senha) VALUES ('Admin', 'admin', 'admin@gmail.com', 'admin');
+INSERT INTO usuario(nome_completo, nome_usuario, email, senha) VALUES ('Douglas Baumgratz', 'doug', 'douglas@gmail.com', '123');
+INSERT INTO usuario(nome_completo, nome_usuario, email, senha) VALUES ('Jonas', 'jon', 'jonas@gmail.com', '123');
+INSERT INTO usuario(nome_completo, nome_usuario, email, senha) VALUES ('Allan', 'all', 'allan@gmail.com', '123');
 
-INSERT INTO usuario(nome_completo, nome_usuario, email, senha) 
-    VALUES ('Admin', 'admin', 'admin@gmail.com', 'admin');
+INSERT INTO promocao(codigo, desconto) VALUES ('COMPRAEMDOBRO', 0.5);
+INSERT INTO promocao(codigo, desconto) VALUES ('DEZPORCENTO', 0.9);
 
-INSERT INTO usuario(nome_completo, nome_usuario, email, senha) 
-    VALUES ('Douglas Baumgratz', 'doug', 'douglas@gmail.com', '123');
-
-INSERT INTO usuario(nome_completo, nome_usuario, email, senha) 
-    VALUES ('Jonas', 'jon', 'jonas@gmail.com', '123');
-
-INSERT INTO usuario(nome_completo, nome_usuario, email, senha) 
-    VALUES ('Allan', 'all', 'allan@gmail.com', '123');
-
-
-INSERT INTO promocao(codigo, desconto) 
-	VALUES ('COMPRAEMDOBRO', 0.5)
-
-INSERT INTO promocao(codigo, desconto) 
-	VALUES ('DEZPORCENTO', 0.9	)
-
-
-INSERT INTO pedido(id_usuario, titulo, descricao, estado, promocao) 
-    VALUES (1, 'X-Bacon', 'Sem salada e sem maionese','Pendente', 'FRETE GRATIS');
-
-INSERT INTO pedido(id_usuario, titulo, descricao, estado, promocao) 
-    VALUES (2, 'X-Salada', 'Sem alface','Em Produção', 'COMPRA EM DOBRO');
-
-INSERT INTO pedido(id_usuario, titulo, descricao, estado, promocao)  
-    VALUES (3, 'Hamburguer Simples', 'Completo','Encaminhado', '');
-
-INSERT INTO pedido(id_usuario, titulo, descricao, estado, promocao)  
-    VALUES (4, 'X-Egg', 'Completo','Entregue', 'FRETE GRATIS');
-
-INSERT INTO pedido(id_usuario, titulo, descricao, estado, promocao)  
-    VALUES (4, 'X-egg', 'Completo','Cancelado', 'FRETE GRATIS');
+INSERT INTO pedido(id_usuario, id_promocao, titulo, estado) VALUES (1, 1, 'X-Bacon', 'Pendente');
+INSERT INTO pedido(id_usuario, id_promocao, titulo, estado) VALUES (2, 2, 'X-Salada', 'Em Produção');
+INSERT INTO pedido(id_usuario, id_promocao, titulo, estado) VALUES (3, 1, 'Hamburguer Simples', 'Encaminhado');
+INSERT INTO pedido(id_usuario, id_promocao, titulo, estado) VALUES (4, 2, 'X-Egg', 'Entregue');
+INSERT INTO pedido(id_usuario, id_promocao, titulo, estado) VALUES (4, 1, 'X-egg', 'Cancelado');
