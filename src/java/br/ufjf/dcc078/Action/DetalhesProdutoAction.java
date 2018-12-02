33201b3cb04abc430f495a80650231b1b8e2a187
J. Gabriel Maia <jonagabriel@gmail.com>
2018-12-02 15:32:49 -0200

    Classe Ociosa: Lista de Pedidos e Lista de Produtos

--- src/java/br/ufjf/dcc078/Action/DetalhesProdutoAction.java
+++ src/java/br/ufjf/dcc078/Action/DetalhesProdutoAction.java
@@ -1,12 +1,10 @@
 package br.ufjf.dcc078.Action;
 
 import br.ufjf.dcc078.Modelo.Componente;
-import br.ufjf.dcc078.Modelo.ListaDeProdutos;
 import br.ufjf.dcc078.Modelo.Produto;
 import br.ufjf.dcc078.Persistencia.ComponenteDAO;
 import br.ufjf.dcc078.Servlet.Action;
 import java.io.IOException;
-import java.util.List;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
@@ -15,7 +13,6 @@ import javax.servlet.http.HttpServletResponse;
 public class DetalhesProdutoAction implements Action{
 
     private RequestDispatcher despachante;
-    private final List<Produto> produtos = ListaDeProdutos.getInstance();
     
     @Override
     public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException, ClassNotFoundException {
